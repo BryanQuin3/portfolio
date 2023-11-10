e4e4b4be4ac64fe54f3020d1cfba05231eed055d
Bryan Quintana <bryandise@gmail.com>
2023-11-10 17:51:50 -0300

    Add react-intersection-observer package and update ProjectsSection animation

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -3,7 +3,9 @@ import { projects } from "../contstants/constants";
 import { ProjectCard } from "./ProjectCard";
 export const ProjectsSection = () => {
   return (
-    <section className="fadeInDown mt-10 grid place-items-center gap-4 px-6 md:grid-cols-2 lg:px-0 ">
+    <section
+      className={`mt-14 grid place-items-center gap-4 px-6 md:grid-cols-2 lg:px-0`}
+    >
       {projects.map((project) => {
         return <ProjectCard key={project.id} project={project} />;
       })}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ export default {
       textWrap: {
         balance: "balance",
       },
+      animation: {
+        fadeInDown: "fadeInDown 1s ease-out",
+      },
+      keyframes: {
+        fadeInDown: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
     },
   },
   plugins: [],
