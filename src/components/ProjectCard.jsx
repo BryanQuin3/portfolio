b65f81fe048533228137d120b5b49a78e9ba67b5
Bryan Quintana <bryandise@gmail.com>
2023-11-14 20:00:00 -0300

    Refactor hover styles in ProjectCard component

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -11,7 +11,7 @@ export const ProjectCard = ({ project }) => {
   return (
     <article
       ref={cardRef}
-      className={`opacity-0 flex items-center flex-col justify-center gap-4 p-8 h-full  rounded-xl bg-gradient-to-r from-gray-100 via-green-50 to-amber-50 border  hover:shadow-sm hover:cursor-pointer`}
+      className={`opacity-0 flex items-center flex-col justify-center gap-4 p-8 h-full  rounded-xl bg-gradient-to-r from-gray-100 via-green-50 to-amber-50 border  hover:shadow-sm`}
     >
       <header className="flex items-center gap-2 justify-between w-full">
         <h3 className={`text-2xl font-semibold text-black`}>{project.name}</h3>
@@ -22,7 +22,7 @@ export const ProjectCard = ({ project }) => {
           className="group bg-white p-3 rounded-full self-start hover:cursor-pointer hover:bg-gray-900 transition-colors duration-300"
           rel="noreferrer"
         >
-          <ArrowOutward className="group-hover:fill-white transition-colors duration-300" />
+          <ArrowOutward className="group-hover:fill-white hover:cursor-pointer transition-colors duration-300" />
         </a>
       </header>
       <p className={`text-wrap-balance text-black`}>{project.description}</p>
@@ -32,7 +32,7 @@ export const ProjectCard = ({ project }) => {
           onMouseMove={(e) => handleMouseMove(e, ref.current)}
           onMouseLeave={() => handleMouseLeave(ref.current)}
           loading="lazy"
-          className="rounded-lg w-full"
+          className="rounded-lg w-full hover:cursor-pointer"
           src={project.img.src}
           alt={project.img.alt}
         />
