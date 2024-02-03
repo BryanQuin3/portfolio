1608634d57ab2fd3ebf398c96d8312aa199fdcac
Bryan Quintana <bryandise@gmail.com>
2024-02-03 16:53:03 -0300

    Refactor Tecnologies component to remove unnecessary CSS classes

--- src/components/Tecnologies.jsx
+++ src/components/Tecnologies.jsx
@@ -3,10 +3,7 @@ export const Tecnologies = ({ tecnologies }) => {
   return (
     <ul className="flex items-center gap-2 flex-wrap">
       {tecnologies.map((tag, index) => (
-        <li
-          key={index}
-          className="rounded-full bg-gradient-to-r from-gray-100 via-green-50 to-amber-50 border px-2 py-1 font-semibold md:px-4"
-        >
+        <li key={index} className="px-2 py-1 font-semibold md:px-4">
           <span>{tag}</span>
         </li>
       ))}
