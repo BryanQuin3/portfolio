2527681fbebcfa7b1756cb97ae336a6c04c725bc
via-lactea336 <bryan.quintana@fiuni.edu.py>
2026-07-21 12:52:58 -0300

    Refactor ProfileSection component to simplify styling and improve image presentation

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -1,13 +1,12 @@
 export const ProfileSection = () => {
   return (
     <figure
-      className="col-span-2 rounded-3xl overflow-hidden bg-cover bg-center"
-      style={{ backgroundImage: "url('./images/bg.webp')" }}
+      className="col-span-2 rounded-3xl overflow-hidden"
     >
       <img
         src="./images/my-profile.webp"
         alt="Bryan Quintana"
-        className="m-auto h-64  md:h-96 lg:h-full"
+        className="m-auto h-64 w-full object-cover object-top md:h-96 lg:h-full"
       />
     </figure>
   );
